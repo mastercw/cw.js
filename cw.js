f6f1ed5be7e4af10cb73f714dc3a0208f42c3a48
Yuval Adam <_@yuv.al>
2020-06-09 14:11:13 +0300

    Initial prettier run

--- cw.js
+++ cw.js
@@ -1,9 +1,9 @@
 function tdit(wpm) {
-    return 60 / (50 * wpm)
+  return 60 / (50 * wpm);
 }
 
 function tfdit(wpm, fwpm) {
-    return ((300 * wpm) - (186 * fwpm)) / (95 * wpm * fwpm)
+  return (300 * wpm - 186 * fwpm) / (95 * wpm * fwpm);
 }
 
-module.exports = {tdit, tfdit};
+module.exports = { tdit, tfdit };
--- test/test.js
+++ test/test.js
@@ -1,21 +1,21 @@
-var cw = require('../cw');
+var cw = require("../cw");
 
-var assert = require('assert');
+var assert = require("assert");
 
-describe('cw', function() {
-    describe('#tdit()', function() {
-        it('should calculate the correct dit length in ms', function() {
-            assert.equal(cw.tdit(20), 0.06);
-            assert.equal(cw.tdit(50), 0.024);
-        });
-    })
-})
+describe("cw", function () {
+  describe("#tdit()", function () {
+    it("should calculate the correct dit length in ms", function () {
+      assert.equal(cw.tdit(20), 0.06);
+      assert.equal(cw.tdit(50), 0.024);
+    });
+  });
+});
 
-describe('cw', function() {
-    describe('#tfdit()', function() {
-        it('should calculate the correct farnsworth dit length in ms', function() {
-            assert.equal(cw.tfdit(20, 10), 0.21789473684210525);
-            assert.equal(cw.tfdit(20, 20), 0.06);
-        })
-    })
-})
+describe("cw", function () {
+  describe("#tfdit()", function () {
+    it("should calculate the correct farnsworth dit length in ms", function () {
+      assert.equal(cw.tfdit(20, 10), 0.21789473684210525);
+      assert.equal(cw.tfdit(20, 20), 0.06);
+    });
+  });
+});
