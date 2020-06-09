c9effce2286d3c65d45fdc44c3900ce336840b22
Yuval Adam <_@yuv.al>
2020-06-09 17:54:09 +0300

    Complete gulpfile tasks build and terse

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const browserify = require("browserify");
 const gulp = require("gulp");
-var source = require("vinyl-source-stream");
+const { series } = require("gulp");
+const terser = require("gulp-terser");
+const rename = require("gulp-rename");
+const source = require("vinyl-source-stream");
 
 gulp.task("build", function () {
   return browserify("src/index.js", {
@@ -8,6 +11,15 @@ gulp.task("build", function () {
   })
     .bundle()
     .pipe(source("bundle.js"))
-    .pipe(gulp.dest("dist/"))
-    .pipe(gulp.dest("examples/"));
+    .pipe(gulp.dest("dist/"));
 });
+
+gulp.task("terse", function () {
+  return gulp
+    .src("dist/bundle.js")
+    .pipe(terser())
+    .pipe(rename({ extname: ".min.js" }))
+    .pipe(gulp.dest("dist/"));
+});
+
+exports.default = series("build", "terse");
