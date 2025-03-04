413e9965b012411dadb50df9d4fb82b3e1938a6c
Yuval Adam <_@yuv.al>
2025-03-04 09:46:03 +0100

    Fix tfdit() optional argument and refactor tests

--- test/test.ts
+++ test/test.ts
@@ -1,28 +1,29 @@
-import { tdit, tfdit, codes } from "../src/index.js";
 import { expect } from "chai";
+// Import the compiled JavaScript
+import * as cw from "../dist/cw.esm.js";
 
 describe("cw", function () {
   describe("#tdit()", function () {
     it("should calculate the correct dit length in ms", function () {
-      expect(tdit(20)).to.equal(0.06);
-      expect(tdit(50)).to.equal(0.024);
+      expect(cw.tdit(20)).to.equal(0.06);
+      expect(cw.tdit(50)).to.equal(0.024);
     });
   });
 
   describe("#tfdit()", function () {
     it("should calculate the correct farnsworth dit length in ms", function () {
-      expect(tfdit(20, 10)).to.equal(0.21789473684210525);
-      expect(tfdit(20, 20)).to.equal(0.06);
-      expect(tfdit(20)).to.equal(0.06);
+      expect(cw.tfdit(20, 10)).to.equal(0.21789473684210525);
+      expect(cw.tfdit(20, 20)).to.equal(0.06);
+      expect(cw.tfdit(20)).to.equal(0.06);
     });
   });
 
   describe("#codes", function () {
     it("should provide an object mapping characters to morse code", function () {
-      expect(codes["C"]).to.equal("-.-.");
-      expect(codes["8"]).to.equal("---..");
-      expect(codes["."]).to.equal(".-.-.-");
-      expect(codes["w"]).to.be.undefined;
+      expect(cw.codes["C"]).to.equal("-.-.");
+      expect(cw.codes["8"]).to.equal("---..");
+      expect(cw.codes["."]).to.equal(".-.-.-");
+      expect(cw.codes["w"]).to.be.undefined;
     });
   });
 });
